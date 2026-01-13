d3e34e70dbe2ca56f95e5d9650a401ec91c642ef
melikbugra <melikbugraozcelik@gmail.com>
2026-01-14 00:26:21 +0300

    fx

--- financia/ui/src/main.jsx
+++ financia/ui/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { WebSocketProvider } from './contexts/WebSocketContext.jsx'
 import { ToastProvider } from './contexts/ToastContext.jsx'
 
 createRoot(document.getElementById('root')).render(
